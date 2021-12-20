e0e15e94205be87e7c0c34942ab5e39fcff4e2d2
Türkay Ürkmez <turkay@turkayurkmez.com>
2021-12-20 13:41:30 +0300

    intro to binding concept

--- introAngular/src/app/app.component.ts
+++ introAngular/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TodoItem } from './models/todo-item.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'introAngular';
+  title : string = 'introAngular';
+  creator : string = 'Türkay Ürkmez'
+  isChecked: boolean = true;
+
+  items:string[] =[
+    'Fethiye','Eskişehir','İzmir'
+  ] 
+
+  item:TodoItem = new TodoItem('çiçek sula',false);
 }
---
+++ introAngular/src/app/models/todo-item.model.ts
@@ -0,0 +1,11 @@
+export class TodoItem {
+    // public task: string | undefined;
+    // public isDone: boolean = false;
+
+    constructor(public task?:string,public isDone?:boolean ) {
+        // this.isDone=isDone;
+        // this.task =task;
+
+    }
+
+}
