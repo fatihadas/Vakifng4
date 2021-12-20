60c5681da95d264e9fe97fc75948be1e372eee5a
Türkay Ürkmez <turkay@turkayurkmez.com>
2021-12-20 16:09:51 +0300

    last changing

--- introAngular/src/app/app.component.ts
+++ introAngular/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { TodoItem } from './models/todo-item.model';
+import { items } from './models/todo-items.mock';
 
 @Component({
   selector: 'app-root',
@@ -9,14 +10,34 @@ import { TodoItem } from './models/todo-item.model';
 export class AppComponent {
   title : string = 'introAngular';
   creator : string = 'Türkay Ürkmez'
-  isChecked: boolean = true;
+  isChecked: boolean = true; 
 
-  items:TodoItem[] =[
-    new TodoItem("Dolar al", false),
-    new TodoItem("BTC al",true),
-    new TodoItem('Angular Öğren',false),
-    new TodoItem('Yurtdışı remote iş bul',false)
-  ] 
+
+  items:TodoItem[] = items;
+
+  isAllTaskShowing:boolean = true;
 
   //item:TodoItem = new TodoItem('çiçek sula',false);
+  getIncompletedTaskCount():number{
+     return this.items.filter(x=>!x.isDone).length;
+  }
+
+  filter():void{
+      this.isAllTaskShowing = !this.isAllTaskShowing;
+      !this.isAllTaskShowing ? this.items = this.items.filter(x=>!x.isDone):
+                               this.items= items;
+      
+  }
+  getFilterButtonText():string{
+    if (this.isAllTaskShowing) {
+      return "Yapılmamış işleri göster";
+    }
+    else{
+      return "Tümünü göster";
+    }
+  }
+
+  addNewItem(task:string):void{
+    this.items.push(new TodoItem(task,false));
+  }
 }
---
+++ introAngular/src/app/models/todo-items.mock.ts
@@ -0,0 +1,8 @@
+import { TodoItem } from "./todo-item.model";
+
+export const items: TodoItem[] = [
+    new TodoItem("Dolar al", false),
+    new TodoItem("BTC al", true),
+    new TodoItem('Angular Öğren', false),
+    new TodoItem('Yurtdışı remote iş bul', false)
+]; 
